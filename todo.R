8de081c01031fd548d4761c5cc13ae2342919b17
misrori <ormraat.pte@gmail.com>
2018-10-02 16:24:59 +0200

    todo

---
+++ todo.R
@@ -0,0 +1,28 @@
+
+#read the house data
+#create data.table object of it
+#check the structure and check the datatypes of the columns
+# how many unique cities exist in the data
+# how many observation does not have postal code
+#how many sale house has more than 100 m^2 balkony
+
+#what is the average price in Pécs for a renewed house, for sale(not for rent)
+# what is the most comon heating in good condition houses
+# which house has the higher price in the highest floor? What is the listing type of it?
+# create a new column which tells the active day (it already has this column)
+# in which city, which heating type house has the biggest balcony area on average
+# what is the property condition where there are the most elevator on average
+# which month has the most house for sale
+
+#plot 
+#use ggplot2
+
+#plot the how many observation can you find by heating type and for listing type(x<- heating type, y<-number by categories, shape <- listing type )
+
+# do a similair plot
+# https://stackoverflow.com/questions/1330989/rotating-and-spacing-axis-labels-in-ggplot2
+
+
+# plot the average balcony sice by country and heating type (heating type should be the collor), add title and format the axis labels
+
+
