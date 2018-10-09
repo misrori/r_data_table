30587c68118325492c90770ffeabaf014895188b
misrori <ormraat.pte@gmail.com>
2018-10-09 14:56:37 +0200

    new tasks

---
+++ tasks.R
@@ -0,0 +1,33 @@
+
+library(data.table)
+library(ggplot2)
+
+bookings <- fread('http://bit.ly/CEU-R-hotels-2018-prices')
+features <- fread('http://bit.ly/CEU-R-hotels-2018-features')
+
+
+# how many uniq country has the dataset
+
+# plot the average distance by stars with ggplot2 bar plot
+
+# merge the dataset into hotels data.table
+
+# which city has the most unique neighbourhood
+
+# create a new variable which is the year and month togather 2017-04  it should be character
+
+# which hotel which month was the most expansive the most expensive on average 
+
+# crete a new data.table contaning the hotels, if the city starts with letter 'B'
+
+# in which country has the most expensive hotels on average
+
+# which city has the best rating and from how many observation
+
+# plot the ratings and the mean price of the ratings
+
+# which city has it's hotels closest to the city center
+
+
+
+
